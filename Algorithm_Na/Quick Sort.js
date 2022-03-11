11d54810ceee0da94a0b0dd24e8480f5a7fd8f09
Shawn Oh <shawnoh8@korea.ac.kr>
2022-03-11 08:47:57 +0000

    Fixed
    Refer to stackoverflow written under the code

--- Algorithm_Na/Quick Sort.js
+++ Algorithm_Na/Quick Sort.js
@@ -8,45 +8,48 @@ const unsortedArray = [1, 10, 5, 8, 7, 6, 4, 3, 2, 9];
 let initialIndex, finalIndex, i, j;
 
 function quickSort(initialIndex, finalIndex, unsortedArray) {
-    console.log(unsortedArray);
-    if(initialIndex == finalIndex) {
-        return;
+    if(initialIndex >= finalIndex) {
+        return unsortedArray;
     }
 
-    let pivot = unsortedArray[initialIndex];
+    let key = initialIndex;
     let i = initialIndex + 1;
     let j = finalIndex;
 
     while(i <= j) {
 
-        while (unsortedArray[i]<=pivot && i<finalIndex) {
+        while (i<=finalIndex && unsortedArray[i]<=unsortedArray[key]) {
             i++;
         }
-        
-        while (unsortedArray[j]>pivot && j>initialIndex) {
+
+        while (j>initialIndex && unsortedArray[j]>=unsortedArray[key]) {
             j--;
         }
     
-        if(i < j) {
+        if(i > j) {
+            let temp = unsortedArray[initialIndex];
+            unsortedArray[initialIndex] = unsortedArray[j];
+            unsortedArray[j] = temp;
+        } else {
             let temp = unsortedArray[i];
             unsortedArray[i] = unsortedArray[j];
             unsortedArray[j] = temp;
-        } else {
-            let temp = unsortedArray[initialIndex];
-            unsortedArray[initialIndex] = unsortedArray[j];
-            unsortedArray[j] = unsortedArray[initialIndex];
         }
-
+        
     }
     console.log(unsortedArray);
-
     quickSort(initialIndex,j-1,unsortedArray);
     quickSort(j+1,finalIndex,unsortedArray);
+    return unsortedArray
 }
 
 function showSortedArray (unsortedArray) {
-    quickSort(0, unsortedArray.length, unsortedArray);
+    quickSort(0, unsortedArray.length-1, unsortedArray);
     console.log(unsortedArray);
 }
 
 showSortedArray(unsortedArray);
+
+// Stackoverflow helped : https://stackoverflow.com/questions/71426308/trying-to-write-quick-sort-algorithm-without-making-2-new-lists-and-appending-c
+// Learned : at line 21, if you compare the value of array first instead of verifying index is not out of bounds
+// you could get an error. Therefore, needs to compare index first. 
